37616bbe7ea7f0e4d2a6c4bacf08dcaf361b7c24
Silas Della Contrada <me@silenium-dev.net>
2024-07-26 18:53:36 +0200

    build: fix publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,14 +35,18 @@ java {
 }
 
 allprojects {
-    repositories {
-        maven(System.getenv("REPOSILITE_URL") ?: "https://reposilite.silenium.dev/snapshots") {
-            name = "reposilite"
-            credentials {
-                username =
-                    System.getenv("REPOSILITE_USERNAME") ?: project.findProperty("reposiliteUser") as String? ?: ""
-                password =
-                    System.getenv("REPOSILITE_PASSWORD") ?: project.findProperty("reposilitePassword") as String? ?: ""
+    apply<MavenPublishPlugin>()
+
+    publishing {
+        repositories {
+            maven(System.getenv("REPOSILITE_URL") ?: "https://reposilite.silenium.dev/snapshots") {
+                name = "reposilite"
+                credentials {
+                    username =
+                        System.getenv("REPOSILITE_USERNAME") ?: project.findProperty("reposiliteUser") as String? ?: ""
+                    password =
+                        System.getenv("REPOSILITE_PASSWORD") ?: project.findProperty("reposilitePassword") as String? ?: ""
+                }
             }
         }
     }
