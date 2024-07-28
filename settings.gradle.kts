04971a42105e57948fdf90cea8551205081aeb6b
Silas Della Contrada <me@silenium-dev.net>
2024-07-28 03:49:38 +0200

    build: exclude subproject if native disabled

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,8 @@
 rootProject.name = "ffmpeg-static"
-include(":ffmpeg")
+
+val deployNative = if (extra.has("deploy.native")) {
+    extra.get("deploy.native")?.toString()?.toBoolean() ?: true
+} else true
+if (deployNative) {
+    include(":ffmpeg")
+}
